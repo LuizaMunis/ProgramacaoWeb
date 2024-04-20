287a18e71fa589b9aee214f3e8bb2fa5a7fc403d
LuizaMunis <luizamunisnascimento@gmail.com>
2024-04-19 23:42:23 -0300

    atualizar

--- 1702-02/index.js
+++ 1702-02/index.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, 'public')));-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+    });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na
@@ -14,92 +14,64 @@ porta ${PORT}`));
 
 let matilha =[];
 
-app.get('/inserir', (req, res) => {
-
-    class cachorro{
-        constructor(nome, idade, peso){
-            this.nome=nome;
-            this.idade=idade;
-            this.peso=peso;
-            this.porte=this.verificarPorte();
-        }
-
-        verificarPorte(){
-        var idadeSemanas = this.idade * 52;
-        var tamPorte = (this.peso / idadeSemanas) * 52;
-
-        if (tamPorte > 45) {
-            return 'gigante';
-        } else if (tamPorte >25 && tamPorte< 45) {
-            return 'grande';
-        } else if(tamPorte>15 && tamPorte<25) {
-            return 'medio';
-        }else if(tamPorte>6 && tamPorte<15) {
-            return 'pequeno';
-        }
-        }
+class cachorro {
+    constructor(nome, idade, peso,porte) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.porte=porte;
+        console.log(nome,idade,peso,porte);
+        
     }
 
+}
+
+function  verificarPorte(peso, idade) {
+    var idadeSemanas = idade * 52;
+    var tamPorte = (peso / idadeSemanas) * 52;
+
+    if (tamPorte > 45) {
+        return 'gigante';
+    } else if (tamPorte > 25 && tamPorte < 45) {
+        return 'grande';
+    } else if (tamPorte > 15 && tamPorte < 25) {
+        return 'medio';
+    } else {
+        return 'pequeno';
+    } 
+}
+
+app.get('/inserir', (req, res) => {
+
+    
     const nome = req.query.nome;
     const idade = parseInt(req.query.idade);
     const peso = parseFloat(req.query.peso);
 
-    const objetoCachorro = new cachorro(nome, idade, peso);
-    verificarPorte();
+
+    let porte=verificarPorte(peso, idade);
+    const objetoCachorro = new cachorro(nome, idade, peso, porte);
+
     matilha.push(objetoCachorro);
 
+    console.log(matilha);
+    let listaHTML = '<ul>';
 
-    let listaHTML = '';
-
-    if (cachorro.porte === "Gigante") {
-
-        let listaHTML = '<ul>';
-        matilha.forEach(cachorro => {
-            if (cachorro.porte === "Gigante") {
-                listaHTML += `<li>${cachorro.nome} /n Idade: ${cachorro.idade} anos, /n Peso: ${cachorro.peso} </li>`;
-            }
-        });
-        listaHTML += '</ul>';
-    }
+    matilha.forEach(cachorro => {
+        if (cachorro.porte === "gigante") {
+                listaHTML += `<li>Nome:${cachorro.nome}  Idade: ${cachorro.idade} anos, Peso: ${cachorro.peso} kg, Porte ${cachorro.porte}</li>`;
+        } else if (cachorro.porte === "grande") {
+                listaHTML += `<li>Nome:${cachorro.nome} Idade: ${cachorro.idade} anos, Peso: ${cachorro.peso} kg, Porte ${cachorro.porte}</li>`;
+        } else if (cachorro.porte === "medio") {
+                listaHTML += `<li>Nome:${cachorro.nome} Idade: ${cachorro.idade} anos, Peso: ${cachorro.peso} kg, Porte ${cachorro.porte}</li>`;
+        } else{
+                listaHTML += `<li>Nome:${cachorro.nome}  Idade: ${cachorro.idade} anos, Peso: ${cachorro.peso} kg, Porte ${cachorro.porte}</li>`;
+        }
+    });
     
-    if (cachorro.porte === "grande") {
-        let listaHTML = '<ul>';
-        matilha.forEach(cachorro => {
-            if (cachorro.porte === "grande") {
-                listaHTML += `<li>${cachorro.nome} - Idade: ${cachorro.idade} anos, Peso: ${cachorro.peso} kg</li>`;
-            }
-        });
-        listaHTML += '</ul>';
-        console.log('Cachorros de porte Grande:');
-        console.log(listaHTML);
-    }
-
-   
-    if (cachorro.porte === "medio") {
-        let listaHTML = '<ul>';
-        matilha.forEach(cachorro => {
-            if (cachorro.porte === "medio") {
-                listaHTML += `<li>${cachorro.nome} - Idade: ${cachorro.idade} anos, Peso: ${cachorro.peso} kg</li>`;
-            }
-        });
-        listaHTML += '</ul>';
-        console.log('Cachorros de porte Médio:');
-        console.log(listaHTML);
-    }
-
-    if (cachorro.porte === "pequeno") {
-        let listaHTML = '<ul>';
-        matilha.forEach(cachorro => {
-            if (cachorro.porte === "pequeno") {
-                listaHTML += `<li>${cachorro.nome} - Idade: ${cachorro.idade} anos, Peso: ${cachorro.peso} kg</li>`;
-            }
-        });
         listaHTML += '</ul>';
     
-    }
-
-    res.send(listaHTML);
+        res.send(listaHTML);
+    });
     
-
-
-})
+    
